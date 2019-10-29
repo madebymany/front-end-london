3ea236fadbae66648b8d2f9959bcee806baf8ccd
Gareth Cozens <gareth@madebymany.co.uk>
2019-10-29 14:28:03 +0000

    Remove offline plugin until build is resolved.

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,8 +84,11 @@ module.exports = {
         siteSpeedSampleRate: 10,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [`/`],
+    //   },
+    // },
   ],
 }
